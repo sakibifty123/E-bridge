c2ec437a31cf0197bb90e5181023d69a0169d623
sakibifty123 <42875960+sakibifty123@users.noreply.github.com>
2020-08-15 20:36:42 +0600

    E-bridge

---
+++ app/src/main/java/com/example/ebridge_aust_03/Add_question_paper.java
@@ -0,0 +1,14 @@
+package com.example.ebridge_aust_03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_question_paper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_question_paper);
+    }
+}
